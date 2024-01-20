2108b388809c02b57bbef5789e0f335b89707231
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-01-20 23:49:53 +0500

    This dart file is added for light and ddark modde

---
+++ lib/res/dark_light_mode/light_mode.dart
@@ -0,0 +1,13 @@
+
+
+import 'package:flutter/material.dart';
+
+ThemeData lightMode = ThemeData(
+  colorScheme: ColorScheme.light(
+    background: Colors.grey.shade300,
+    primary: Colors.grey.shade500,
+    secondary: Colors.grey.shade200,
+    tertiary: Colors.white,
+    inversePrimary: Colors.grey.shade900
+  )
+);

68a21499f779caad420e5f1553874cee25cf21cc
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-01-20 23:51:47 +0500

    this is texxt button custom widgget

---
+++ lib/res/widgets/my_text_button.dart
@@ -0,0 +1,41 @@
+import 'package:chat_app/res/widgets/my_text.dart';
+import 'package:flutter/material.dart';
+
+class MyTextButton extends StatelessWidget {
+  final String title;
+  final Color? backgroundColor;
+  final double? fontSize;
+  final Color? textColor;
+  final FontWeight? fontWeight;
+  final double? width;
+  final double? height;
+  final void Function()? onTap;
+
+  const MyTextButton({
+    super.key,
+    required this.title,
+    this.fontSize,
+    this.fontWeight,
+    this.textColor,
+    this.backgroundColor,
+    required this.onTap,
+    this.width = double.infinity,
+    this.height = 40.0,});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        alignment: Alignment.center,
+        width: width,
+        height: height,
+        decoration: BoxDecoration(
+          color: backgroundColor,
+          borderRadius: BorderRadius.circular(8)
+        ),
+        child: MyText(title: title,fontSize: fontSize,fontWeight: fontWeight,color: textColor,),
+      ),
+    );
+  }
+}

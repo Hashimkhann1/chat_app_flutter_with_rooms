89add6506463e3b640334778507f4808853cd08f
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-01-22 21:21:22 +0500

    updated

---
+++ lib/res/widgets/my_text.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class MyText extends StatelessWidget {
+  final String title;
+  final Color? color;
+  final double? fontSize;
+  final FontWeight? fontWeight;
+  final TextAlign? textAlign;
+  const MyText(
+      {super.key,
+      required this.title,
+      this.color,
+      this.fontSize,
+      this.fontWeight,
+        this.textAlign,
+      });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      title,
+      textAlign: textAlign,
+      style:
+          TextStyle(
+              color: color,
+              fontSize: fontSize,
+              fontWeight: fontWeight,
+          ),
+    );
+  }
+}

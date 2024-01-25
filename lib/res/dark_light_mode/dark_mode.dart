d3c3dc9504c2b723591ed5d1de690ea9d358d06c
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-01-25 22:06:21 +0500

    in this file Theme color is added for dark mode

---
+++ lib/res/dark_light_mode/dark_mode.dart
@@ -0,0 +1,13 @@
+
+
+import 'package:flutter/material.dart';
+
+ThemeData darkmode = ThemeData(
+    colorScheme: ColorScheme.light(
+        background: Colors.grey.shade900,
+        primary: Colors.grey.shade600,
+        secondary: Colors.grey.shade700,
+        tertiary: Colors.grey.shade800,
+        inversePrimary: Colors.grey.shade300
+    )
+);

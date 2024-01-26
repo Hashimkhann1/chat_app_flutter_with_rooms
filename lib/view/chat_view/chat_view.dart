6e4b011d4e5030f07de6b1df7d8eedada34fe8e1
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-01-26 15:48:39 +0500

    updated for scrolling to chat end

--- lib/view/chat_view/chat_view.dart
+++ lib/view/chat_view/chat_view.dart
@@ -5,13 +5,21 @@ import 'package:chat_app/view_model/chat_view_model/chat_view_model.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class ChatView extends StatelessWidget {
+class ChatView extends StatefulWidget {
   final String receiverEmail;
   final String receiverID;
-  ChatView({super.key,required this.receiverEmail,required this.receiverID});
+  ChatView({super.key, required this.receiverEmail, required this.receiverID});
 
+  @override
+  State<ChatView> createState() => _ChatViewState();
+}
+
+class _ChatViewState extends State<ChatView> {
   // text controller
   final TextEditingController _messageController = TextEditingController();
+
+  // textfield focus
+  FocusNode myFocusNode = FocusNode();
 
   //chat & auth services
   final ChatViewModel chatViewModel = ChatViewModel();
@@ -19,10 +27,43 @@ class ChatView extends StatelessWidget {
   final _auth = FirebaseAuth.instance.currentUser!;
 
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    myFocusNode.addListener(() {
+      if (myFocusNode.hasFocus) {
+        Future.delayed(const Duration(milliseconds: 500), () => scrollDown());
+      }
+    });
+
+    Future.delayed(const Duration(milliseconds: 400),() => scrollDown());
+
+  }
+
+
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    myFocusNode.dispose();
+    _messageController.dispose();
+    super.dispose();
+  }
+
+  // scrool controller
+  final ScrollController _scrollController = ScrollController();
+  void scrollDown() {
+    _scrollController.animateTo(_scrollController.position.maxScrollExtent,
+        duration: const Duration(seconds: 1), curve: Curves.fastOutSlowIn);
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const MyText(title: "Chat",),
+        title: const MyText(
+          title: "Chat",
+        ),
         backgroundColor: Colors.transparent,
         foregroundColor: Colors.grey,
         elevation: 0,
@@ -31,34 +72,41 @@ class ChatView extends StatelessWidget {
         padding: const EdgeInsets.symmetric(horizontal: 14.0),
         child: Column(
           children: [
-            const SizedBox(height: 16,),
+            const SizedBox(
+              height: 16,
+            ),
             // getting all messages
             Expanded(
                 child: StreamBuilder(
-                  stream: chatViewModel.getMessages(_auth.uid, receiverID),
-                  builder: (context , snapshot) {
-                    if(snapshot.hasError){
-                      return MyText(title: "Error");
-                    }
-                    else if(snapshot.connectionState == ConnectionState.waiting){
-                      return Center(child: CircularProgressIndicator());
-                    }
-                    return ListView(
-                      children: snapshot.data!.docs.map((doc) {
-                        Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
-                        // checking sender messages
-                        bool isSender = data['senderID'].toString() == _auth.uid.toString();
-                        return Column(
-                          crossAxisAlignment: isSender ? CrossAxisAlignment.end : CrossAxisAlignment.start,
-                          children: [
-                            ChatBubble(isSender: isSender, message: data['message']),
-                          ],
-                        );
-                      }).toList(),
+              stream: chatViewModel.getMessages(_auth.uid, widget.receiverID),
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  return MyText(title: "Error");
+                } else if (snapshot.connectionState ==
+                    ConnectionState.waiting) {
+                  return Center(child: CircularProgressIndicator());
+                }
+                return ListView(
+                  controller: _scrollController,
+                  children: snapshot.data!.docs.map((doc) {
+                    Map<String, dynamic> data =
+                        doc.data() as Map<String, dynamic>;
+                    // checking sender messages
+                    bool isSender =
+                        data['senderID'].toString() == _auth.uid.toString();
+                    return Column(
+                      crossAxisAlignment: isSender
+                          ? CrossAxisAlignment.end
+                          : CrossAxisAlignment.start,
+                      children: [
+                        ChatBubble(
+                            isSender: isSender, message: data['message']),
+                      ],
                     );
-                  },
-                )
-            ),
+                  }).toList(),
+                );
+              },
+            )),
 
             // textfield and send message button
             Padding(
@@ -71,12 +119,14 @@ class ChatView extends StatelessWidget {
                   Expanded(
                     child: TextField(
                       controller: _messageController,
+                      focusNode: myFocusNode,
                       decoration: InputDecoration(
-                        hintText: "Type Your Message",
-                        border: OutlineInputBorder(
-                          borderSide: BorderSide(color: Theme.of(context).colorScheme.secondary)
-                        )
-                      ),
+                          hintText: "Type Your Message",
+                          border: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                  color: Theme.of(context)
+                                      .colorScheme
+                                      .secondary))),
                     ),
                   ),
 
@@ -84,15 +134,23 @@ class ChatView extends StatelessWidget {
                   Container(
                     margin: EdgeInsets.only(left: 12),
                     decoration: BoxDecoration(
-                      color: Colors.green,
-                      shape: BoxShape.circle
-                    ),
-                    child: IconButton(onPressed: (){
-                      if(_messageController.text.isNotEmpty){
-                        chatViewModel.sendMessage(receiverID, _messageController.text.toString());
-                        _messageController.clear();
-                      }
-                    }, icon: Icon(Icons.arrow_upward,size: 30,color: Colors.white,)),
+                        color: Colors.green, shape: BoxShape.circle),
+                    child: IconButton(
+                        onPressed: () {
+                          if (_messageController.text.isNotEmpty) {
+                            chatViewModel.sendMessage(widget.receiverID,
+                                _messageController.text.toString());
+                            _messageController.clear();
+                            print("Scrolling down...");
+                            scrollDown();
+                          }
+                          // scrollDown();
+                        },
+                        icon: Icon(
+                          Icons.arrow_upward,
+                          size: 30,
+                          color: Colors.white,
+                        )),
                   )
                 ],
               ),
